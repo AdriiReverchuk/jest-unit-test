13c7b82d0d3fe61661b1775d98b8d40b8ac41775
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-05-27 23:29:51 +0300

    test

--- js/index.js
+++ js/index.js
@@ -1,44 +1,94 @@
 export class AddItemForm {
-	constructor() {
-		this.createDOM();
-	}
-	createDOM() {
-		const form = this.createElement("form", "container");
-		form.style.padding = "50px";
-		form.style.margin = "0 auto";
-		form.style.width = "20%";
-		const title = this.createElement("h2", "title", "Registration");
-		const emailContainer = this.createElement("div", "emailContainer");
-		const labelEmail = this.createElement("label", "", "Email");
-		labelEmail.setAttribute("for", "email");
-		const inpEmail = this.createElement("input", "email");
-		emailContainer.append(labelEmail, inpEmail);
-		const passwordContainer = this.createElement("div");
-		const labelPassword = this.createElement("label", "", "Password");
-		labelPassword.setAttribute("for", "password");
-		const inpPassword = this.createElement("input", "password");
-		inpPassword.setAttribute("type", "password");
-		passwordContainer.append(labelPassword, inpPassword);
-		const repeatContainer = this.createElement("div");
-		const labelRepPassword = this.createElement("label", "", "Repeat Password");
-		labelRepPassword.setAttribute("for", "rep-password");
-		const inpRepPassword = this.createElement("input", "rep-password");
-		inpRepPassword.setAttribute("type", "password");
-		repeatContainer.append(labelRepPassword, inpRepPassword);
-		const btn = this.createElement("button", "btn", "Create Account");
-		btn.setAttribute("disabled", "disabled");
-		form.append(title, emailContainer, passwordContainer, repeatContainer, btn);
-		document.body.prepend(form);
-	}
-	createElement(tag, id, text) {
-		const elem = document.createElement(tag);
-		if (id) elem.id = id;
-		if (text) elem.textContent = text;
-		return elem;
-	}
-	getElement(selector) {
-		const elem = document.querySelector(selector);
-		return elem;
-	}
+   constructor() {
+      this.createDOM();
+      this.regExpEm = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/;
+      this.regExPass =
+         /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+   }
+   createDOM() {
+      const form = this.createElement("form", "container");
+      form.style.padding = "50px";
+      form.style.margin = "0 auto";
+      form.style.width = "20%";
+      const title = this.createElement("h2", "title", "Registration");
+      const emailContainer = this.createElement("div", "emailContainer");
+      const labelEmail = this.createElement("label", "labelForEmail", "Email");
+      labelEmail.setAttribute("for", "email");
+      const inpEmail = this.createElement("input", "email");
+      inpEmail.setAttribute("type", "email");
+      emailContainer.append(labelEmail, inpEmail);
+      const passwordContainer = this.createElement("div", "passwordContainer");
+      const labelPassword = this.createElement(
+         "label",
+         "labeForPassword",
+         "Password"
+      );
+      labelPassword.setAttribute("for", "password");
+      const inpPassword = this.createElement("input", "password");
+      inpPassword.setAttribute("type", "password");
+      passwordContainer.append(labelPassword, inpPassword);
+      const repeatContainer = this.createElement(
+         "div",
+         "repeatPasswordContainer"
+      );
+      const labelRepPassword = this.createElement(
+         "label",
+         "labelForRepPassword",
+         "Repeat Password"
+      );
+      labelRepPassword.setAttribute("for", "rep-password");
+      const inpRepPassword = this.createElement("input", "rep-password");
+      inpRepPassword.setAttribute("type", "password");
+      repeatContainer.append(labelRepPassword, inpRepPassword);
+      const btn = this.createElement("button", "btn", "Create Account");
+      btn.setAttribute("disabled", "disabled");
+      form.append(
+         title,
+         emailContainer,
+         passwordContainer,
+         repeatContainer,
+         btn
+      );
+      document.body.prepend(form);
+      document.body.addEventListener("keydown", (event) => {
+         if (
+            this.regExpEm.test(inpEmail.value) &&
+            this.regExPass.test(inpPassword.value) &&
+            inpRepPassword.value === inpPassword.value
+         ) {
+            btn.removeAttribute("disabled");
+            if (this.getElement(".message")) {
+               this.getElement(".message").remove();
+            }
+            btn.addEventListener("click", () => {
+               alert(`<p>Ваш eмаil - ${inpEmail.value}</p>
+					<p>Ваш пароль - ${inpPassword.value}</p>`);
+               inpEmail.value = "";
+               inpPassword.value = "";
+               inpRepPassword.value = "";
+            });
+         } else {
+            const message = this.createElement(
+               "p",
+               "message",
+               "пожалуйста проверьте ваш пароль"
+            );
+            message.style.color = "red";
+         }
+      });
+      const regExpEm = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/;
+      const regExPass =
+         /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+   }
+   createElement(tag, id, text) {
+      const elem = document.createElement(tag);
+      if (id) elem.id = id;
+      if (text) elem.textContent = text;
+      return elem;
+   }
+   getElement(selector) {
+      const elem = document.querySelector(selector);
+      return elem;
+   }
 }
 const form = new AddItemForm();
--- js/index.spec.js
+++ js/index.spec.js
@@ -1,25 +1,58 @@
 import { AddItemForm } from "./index.js";
 describe("Add Item Form", () => {
-	it("should create", () => {
-		expect.assertions(1);
-		const form = new AddItemForm();
-		expect(form).toBeTruthy();
-	});
+   it("should create", () => {
+      expect.assertions(1);
+      const form = new AddItemForm();
+      expect(form).toBeTruthy();
+   });
 });
 describe("Layout", () => {
-	it("should generate container", () => {
-		expect.assertions(1);
-		const container = document.getElementById("container");
-		expect(container).toBeTruthy();
-	});
-	it("should generate title", () => {
-		expect.assertions(1);
-		const title = document.getElementById("title");
-		expect(title).toBeTruthy();
-	});
-	it("should generate  emailContainer", () => {
-		expect.assertions(1);
-		const emailContainer = document.getElementById("emailContainer");
-		expect(emailContainer).toBeTruthy();
-	});
+   it("should generate container", () => {
+      expect.assertions(1);
+      const container = document.getElementById("container");
+      expect(container).toBeTruthy();
+   });
+   it("should generate title", () => {
+      expect.assertions(1);
+      const title = document.getElementById("title");
+      expect(title).toBeTruthy();
+   });
+   it("should generate  emailContainer", () => {
+      expect.assertions(3);
+      const emailContainer = document.getElementById("emailContainer");
+      const inpEmail = document.getElementById("email");
+      const labelEmail = document.getElementById("labelForEmail");
+      expect(emailContainer).toBeTruthy();
+      expect(inpEmail).toBeTruthy();
+      expect(labelEmail).toBeTruthy();
+   });
+   it("should generate  paswordContainer", () => {
+      expect.assertions(3);
+      const passwordContainer = document.getElementById("passwordContainer");
+      const inpPassword = document.getElementById("password");
+      const labelPassword = document.getElementById("labeForPassword");
+      expect(passwordContainer).toBeTruthy();
+      expect(inpPassword).toBeTruthy();
+      expect(labelPassword).toBeTruthy();
+   });
+   it("should generate  paswordRepeatContainer", () => {
+      expect.assertions(3);
+      const repeatPasswordContainer = document.getElementById(
+         "repeatPasswordContainer"
+      );
+      const inpRepPassword = document.getElementById("rep-password");
+      const labelRepPassword = document.getElementById("labelForRepPassword");
+      expect(repeatPasswordContainer).toBeTruthy();
+      expect(inpRepPassword).toBeTruthy();
+      expect(labelRepPassword).toBeTruthy();
+   });
+   it("should create Button", () => {
+      expect.assertions(3);
+      const btn = document.getElementById("btn");
+      const atribute = btn.getAttribute("disabled");
+      const labelRepPassword = document.getElementById("labelForRepPassword");
+      expect(btn).toBeTruthy();
+      expect(atribute).toBeTruthy();
+      expect(labelRepPassword).toBeTruthy();
+   });
 });
